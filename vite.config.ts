9d7b8a2a01103917a6a66da142534890ceb0673e
Kevinjeevus <kevinjeevus@gmail.com>
2026-02-17 20:13:19 +0530

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -25,7 +25,7 @@ export default defineConfig(({ mode }) => ({
         display: "standalone",
         orientation: "portrait",
         scope: "/",
-        start_url: "/",
+        start_url: "/dashboard",
         icons: [
           {
             src: "/favicon.png",
